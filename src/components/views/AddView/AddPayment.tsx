19ddecaacb1175c611c354446555d93c478784bc
Milena Pieńkosz <designbymilencja@gmail.com>
2023-03-27 15:47:58 +0100

    added validation date and values for select country and select category in AddPayment

--- src/components/views/AddView/AddPayment.tsx
+++ src/components/views/AddView/AddPayment.tsx
@@ -1,6 +1,8 @@
 import React, {SyntheticEvent, useState} from "react";
 import {Button} from "../../common/Button/Button";
-
+import {useFetchAndLoading} from "../../../hooks/useFetchAndLoading";
+import {CountryEntity} from 'types';
+import {CategoryEntity} from 'types';
 export const AddPayment = () => {
     const [form, setForm] = useState({
         cost: 0,
@@ -9,12 +11,12 @@ export const AddPayment = () => {
         idCountry: '',
         idCategory: '',
     });
-    // useEffect(()=>{
-    // const dTYear = new Date().toLocaleDateString()
-    // console.log(dTYear.replaceAll("/",""))
-    //     setData(form.boughtAt)
-    // }, [form.boughtAt]);
-    // MAX="2020-01-19"
+
+    const [countriesData] = useFetchAndLoading('http://localhost:3001/country');
+    const [categoriesData] = useFetchAndLoading('http://localhost:3001/category');
+    const currencies = countriesData.map((country: CountryEntity) => country.currency);
+    const noDoubleCurrencies = [...new Set(currencies)];
+
     const [status, setStatus] = useState(0)
     const saveNewPayment = async (e: SyntheticEvent) => {
         e.preventDefault();
@@ -45,34 +47,40 @@ export const AddPayment = () => {
     const clearInput = () => {
         setStatus(0);
     }
+    const chosenDate = new Date(form.boughtAt).toLocaleDateString().replaceAll("/", "");
+    const todayDate = new Date().toLocaleDateString().replaceAll("/", "");
+    const isCorrect = Number(chosenDate) - Number(todayDate);
+    console.log(isCorrect)
+
     return <>
         <form className="form" onSubmit={saveNewPayment}>
             <h3 className='add'>Dodaj wydatek</h3>
+            {form.cost > 999999 ? <p className="error">Płatność nie może być wyższa niż milion złotych</p> : null}
             <label>Kwota</label>
             <input type="number" maxLength={999999.99} required value={form.cost}
                    onChange={e => saveForm('cost', e.target.value)} onMouseDown={clearInput}/>
             <label>Waluta</label>
             <select value={form.currency} onChange={e => saveForm('currency', e.target.value)}>
                 <option>--</option>
-                <option>USD</option>
-                <option>EUR</option>
-                <option>PLN</option>
+                {noDoubleCurrencies.map((currency, index)=><option key={index} value={currency}>{currency}</option>)}
             </select>
+            {isCorrect > 0 ? <p className="error">Data płatności nie może być z przyszłości</p> : null}
             <label>Data zakupu</label>
             <input type="date" required value={form.boughtAt}
                    onChange={e => saveForm('boughtAt', e.target.value)}/>
             <label>Miejsce zakupu</label>
             <select value={form.idCountry} onChange={e => saveForm('idCountry', e.target.value)}>
                 <option>--</option>
-                <option>66d809b2-bd08-11ed-8ec8-7e1d1a287df9</option>
+                {countriesData.map((country:CountryEntity)=> <option key={country.name} value={country.id}>{country.name}</option>)}
             </select>
             <label>Kategoria zakupu</label>
             <select name="category" value={form.idCategory} onChange={e => saveForm('idCategory', e.target.value)}>
                 <option>--</option>
-                <option>d336e696-bd08-11ed-8ec8-7e1d1a287df9</option>
+                {categoriesData.map((category:CategoryEntity)=> <option key={category.id} value={category.id}>{category.name}</option>)}
             </select>
             <Button text="Dodaj wydatek" name="btn"/>
             {status === 200 ? <p className="success">Wydatek został dodany pomyślnie</p> : null}
-        </form>
+            {status === 400 ? <p className="error">Wydatek nie został dodany, sprawdź poprawność danych w  formularzu </p> : null }
+                </form>
     </>
 }
