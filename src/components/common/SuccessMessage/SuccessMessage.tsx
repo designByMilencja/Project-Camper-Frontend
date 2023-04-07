237e243ec7260f55700534bbf1096eb9cafe15f1
Milena Pieńkosz <designbymilencja@gmail.com>
2023-04-07 18:32:05 +0100

    added shorter handle of status errors

--- src/components/common/SuccessMessage/SuccessMessage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './SuccessMessage.scss';
-interface Props {
-    message: string;
-}
-
-export const SuccessMessage = ({ message }:Props) => {
-    return <p className="success">{message}</p>;
-}
