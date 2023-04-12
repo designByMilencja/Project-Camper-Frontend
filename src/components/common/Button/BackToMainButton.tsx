370d9ac16dbae2261ed06c3383c94fb29e52c61f
Milena Pieńkosz <designbymilencja@gmail.com>
2023-04-12 20:45:40 +0100

    refactored before upload to server

--- src/components/common/Button/BackToMainButton.tsx
+++ src/components/common/Button/BackToMainButton.tsx
@@ -1,5 +1,5 @@
 import {Button} from "./Button";
 
 export const BackToMainButton = () => {
-    return <Button text="Wróć na stronę główną" to="/" name="center" />;
+    return <Button text="Wróć na stronę główną" to="/" name="center"/>;
 };
--- src/components/common/InputField/InputField.tsx
+++ src/components/common/InputField/InputField.tsx
@@ -10,23 +10,23 @@ interface Props {
     min?: number;
     required?: boolean;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    onMouseDown?: (e:React.ChangeEvent<HTMLInputElement>) => void;
+    onMouseDown?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const InputField =
-    ({label,type,name,value,minLength,maxLength,required=false,onChange}: Props) => {
-return (
-    <>
-    <label>{label}</label>
-        <input
-        type={type}
-        name={name}
-        value={value}
-        minLength={minLength}
-        maxLength={maxLength}
-        onChange={onChange}
-        required={required}
-        />
-    </>
-)
-}
+    ({label, type, name, value, minLength, maxLength, required = false, onChange}: Props) => {
+        return (
+            <>
+                <label>{label}</label>
+                <input
+                    type={type}
+                    name={name}
+                    value={value}
+                    minLength={minLength}
+                    maxLength={maxLength}
+                    onChange={onChange}
+                    required={required}
+                />
+            </>
+        )
+    }
